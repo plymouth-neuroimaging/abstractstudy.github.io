803a43239ea0e994cfd5c01b18b680d658cc2206
wanghongree <hongruiwang1987@gmail.com>
2024-02-14 15:01:49 +0000

    webpage

--- _site/assets/js/lunr-store.js
+++ _site/assets/js/lunr-store.js
@@ -11,9 +11,19 @@ var store = [
         "text": ""
     }, 
     { 
+        "url": "/index%20copy%202.html",
+        "title": null,
+        "text": "ABSTRACT is a pioneering research study which aims to use the power of Artificial Intelligence to make better predictions about who will develop a stroke in the future. By analysing information held in healthcare records and medical tests, we aim to produce personalised assessments of the risk of future stroke, enabling healthcare professionals to offer treatment to reduce this risk. To find out more click here. We would love to hear your views on this project, which you can tell us by completing this feedback survey."
+    }, 
+    { 
+        "url": "/index%20copy.html",
+        "title": null,
+        "text": "SUSTAIN SW is a collaboration between the University of Plymouth and University Hospitals Plymouth NHS Trust. Made up of academic clinicians, data scientists and governance experts, it aims to leverage the unique demographics, geography and healthcare infrastructure of the South West Peninsula to produce data driven solutions to unmet healthcare needs. This means developing technologies which allow earlier diagnosis, enable disease risk stratification and/or improve patient safety. Our projects, which span a range of disease areas, are selected because they have a high chance of making a rapid impact on patient care. Central to our work is the use of routinely collected healthcare data in a transparent, safe and secure manner. We use this data to enable development of models (often using Artificial Intelligence or other advanced bioinformatic/statistical techniques) to improve the performance of healthcare systems, allowing cheaper, better care for patients. To enable this, we have developed a strategic network of collaborators and supporters across the South West Peninsula of the United Kingdom. This network draws on expertise in the academic, healthcare and commercial sectors, to enable development of technologies in the most rapid and efficient way possible. We hope that our work will improve the delivery of healthcare and add to the economic prosperity of the region and the UK."
+    }, 
+    { 
         "url": "/index.html",
         "title": "Home",
-        "text": "SUSTAIN SW is a collaboration between the University of Plymouth and University Hospitals Plymouth NHS Trust. Made up of academic clinicians, data scientists and governance experts, it aims to leverage the unique demographics, geography and healthcare infrastructure of the South West Peninsula to produce data driven solutions to unmet healthcare needs. This means developing technologies which allow earlier diagnosis, enable disease risk stratification and/or improve patient safety. Our projects, which span a range of disease areas, are selected because they have a high chance of making a rapid impact on patient care. Central to our work is the use of routinely collected healthcare data in a transparent, safe and secure manner. We use this data to enable development of models (often using Artificial Intelligence or other advanced bioinformatic/statistical techniques) to improve the performance of healthcare systems, allowing cheaper, better care for patients. To enable this, we have developed a strategic network of collaborators and supporters across the South West Peninsula of the United Kingdom. This network draws on expertise in the academic, healthcare and commercial sectors, to enable development of technologies in the most rapid and efficient way possible. We hope that our work will improve the delivery of healthcare and add to the economic prosperity of the region and the UK."
+        "text": ""
     }, 
     { 
         "url": "/optout.html",
